b604229d54e6e4a1f4d4cc85edaccb2f92192d5b
Jan Robert <janmartinrobert@gmail.com>
2021-07-27 14:11:32 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/studentdb/StudentdbApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.studentdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/studentdb/StudentdbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.studentdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
